4d51b30dd43f86fa38b7146bd924e42e19696f2a
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-04-23 23:49:48 +0530

    Add files via upload

--- to-do-list-assignment/client/scripts.js
+++ to-do-list-assignment/client/scripts.js
@@ -1,3 +1,21 @@
+let arr = [
+    {
+        note: "Hello 1"
+    },
+    {
+        note: "Hello 2"
+    }
+]
+
+const tasksList = document.getElementById('tasksList');
+
+arr.map(task => {
+    const taskElement = document.createElement('div');
+    // taskElement.textContent = task.note;
+    // // tasksList.appendChild(taskElement);
+    console.log(task.note)
+})
+
 const handleSubmit = (e) => {
     e.preventDefault();
 }
