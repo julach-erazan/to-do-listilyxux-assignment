12a996398b06e8b8cec07ad3b0a2a45f24fcaed0
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-04-23 23:35:51 +0530

    update

---
+++ to-do-list-assignment/client/scripts.js
@@ -0,0 +1,16 @@
+const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
+const addTask = (newTask) => {
+    //some codse here
+    console.log(newTask);
+}
+
+const updateTask = () => {
+    //some codse here
+}
+
+const deleteTask = () => {
+    //some codse here
+}
